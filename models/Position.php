51c5ff0621a890524f2707ee7bb641149b7c68be
Asus <voldemapro@hotmail.com>
2017-08-24 21:39:45 +0500

    New Position model with open and close methods

--- models/Position.php
+++ models/Position.php
@@ -4,9 +4,13 @@ namespace app\models;
 use Yii;
 use yii\db\Command;
 use yii\db\ActiveRecord;
+use app\models\Contract;
 
 class Position extends ActiveRecord
 {
+	const BUY_ID = 1;
+	const SELL_ID = -1;
+	
 	public static $types = [1 => 'Покупка', -1 => 'Продажа'];
 	
 	/**
@@ -29,20 +33,6 @@ class Position extends ActiveRecord
         ];
     }
 	
-	public static function formatSum($sum) {
-		return abs($sum) >= 100000
-			? trim(
-				\preg_replace('|^(\d{1,2})?(\d{3})?(\d{3})(\.\d{2,3})?$|', '$1 $2 $3$4', $sum)
-			)
-			: trim(
-				\preg_replace('|^(\d{1,2})?(\d{3})(\.\d{2,3})?$|', '$1 $2 $3', $sum)
-			);
-	}
-	public static function formatSign($sum) {
-		$signPref = [-1 => '<em class="monosign">&ndash;</em>', 0 => '<em class="monosign">&nbsp;</em>', 1 => '<em class="monosign">+</em>'];
-		return floatval($sum) ? $signPref[$sum/abs($sum)] : $signPref[0];
-	}
-	
 	/**
      * @return array Validation rules
      */	
@@ -51,7 +41,7 @@ class Position extends ActiveRecord
 			[['open_quot', 'close_quot', 'result'], 'double'],
 			[['disabled'], 'boolean'],
 			
-			[['open_time', 'open_quot', 'open_sum'], 'required', 'message' => 'Обязательное поле']
+			[['open_time', 'open_quot', 'volume'], 'required', 'message' => 'Обязательное поле']
 		];
 	}
 	
@@ -60,6 +50,85 @@ class Position extends ActiveRecord
 			'user_id'  => 18,
 			'disabled'  => 13
 		];
+	}
+	
+	public function open($type = 1) {
+		if (!($q = Contract::getQuotes()) || empty($q[ STP_VRS ]))
+			return false;
+		else
+			$q = $q[ STP_VRS ];
+		
+		$this = new (self::className());
+		$this->type = $type;
+		$this->open_quot = $this->type > 0 ? $q['ask'] : $q['bid'];
+		$this->open_time = date('Y-m-d H:i:s');
+		$this->volume = floor(Yii::$app->user->identity->credit / $this->open_quot);
+		
+		Yii::$app->user->identity->balance -= $this->volume * $this->open_quot;
+		
+		$conn = \Yii::$app->db;
+		$dbt = $conn->beginTransaction(); 
+
+		try {
+			Yii::$app->user->identity->save();
+			$this->save();
+			$dbt->commit();
+			return true;
+		} catch (\Exception $e) {
+			$dbt->rollBack();
+			throw $e;
+		}	
+	}
+	
+	public function close() {
+		if (!($q = Contract::getQuotes()) || empty($q[ STP_VRS ]))
+			return false;
+		else
+			$q = $q[ STP_VRS ];
+		
+		$this->close_quot = $this->type > 0 ? $q['bid'] : $q['ask'];
+		$this->result = $this->type * ($this->close_quot - $this->open_quot) * $this->volume;
+		$this->close_time = date('Y-m-d H:i:s');
+
+		Yii::$app->user->identity->balance = Yii::$app->user->identity->credit;
+
+		$conn = \Yii::$app->db;
+		$dbt = $conn->beginTransaction(); 
+
+		try {
+			Yii::$app->user->identity->save();
+			$this->save();
+			$dbt->commit();
+			return true;
+		} catch (\Exception $e) {
+			$dbt->rollBack();
+			throw $e;
+		}	
+	}
+	
+	public static function validateOpen() {
+		$allowTrade = time() < Yii::$app->params['close_time'] && time() > Yii::$app->params['open_time'];
+		$allowOpenByTime = $allowTrade && time() < Yii::$app->params['input_before'];
+		$p = Position::find()
+				->where('`user_id` = '.Yii::$app->user->id)
+				->andWhere('DATE(`open_time`) = CURDATE()')
+				->one();
+		if ($p || !$allowOpenByTime)	
+			return false;
+		else
+			return true;
+	}
+	
+	public static function validateClose() {
+		$allowTrade = time() < Yii::$app->params['close_time'] && time() > Yii::$app->params['open_time'];
+		$p = Position::find()
+				->where('`user_id` = '.Yii::$app->user->id)
+				->andWhere('DATE(`open_time`) = CURDATE()')
+				->one();
+		if (!$p || !$allowTrade || (time() - $p->open_time) < 600)	
+			return false;
+		else
+			return $p;
 	}
 }
 ?>
