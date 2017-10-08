457b7e44844d4e21e3845add93435809d7419723
Asus <voldemapro@hotmail.com>
2017-10-08 19:35:24 +0500

    Layout update with style separation and control script change

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,7 +14,7 @@
 	
 	if (empty($summary['stat'])) {
 		$profitabilityFormatted = '<span class="glyphicon glyphicon-credit-card icon-group"></span>';
-		$profitabilityFormatted .= '<span class="icon-group">' . ($user->debit >= 0 ? '' : '&ndash; ') . number_format(abs($user->debit), 0, '.', ' ') . ' р</span>';
+		$profitabilityFormatted .= '<span class="icon-group">' . ($user->debit >= 0 ? '' : '&ndash; ') . round(abs($user->debit), 2) . ' <span class="glyphicon glyphicon-ruble"></span></span>';
 	} else {
 		$stat_perc = $summary['stat']['total'] ? round($summary['stat']['success'] / $summary['stat']['total'] * 100) : 0;
 		$profitabilityFormatted = '<span class="glyphicon glyphicon-stats icon-group"></span>';
@@ -38,7 +38,7 @@
 
 		<link rel="stylesheet" href="/styles/bootstrap.min.css" type="text/css" />
 		<link rel="stylesheet" href="/styles/jquery-ui.min.css" type="text/css" />
-		<link rel="stylesheet" href="//sotabank.com/styles/common.css" type="text/css" /><?php
+		<link rel="stylesheet" href="/styles/stp.css?x=<?php echo filemtime(Yii::getAlias('@app/web/styles/stp.css')) ?>" type="text/css" /><?php
 
 		?> 
 
@@ -55,135 +55,21 @@
 		<![endif]-->
 		<script>
 			var config = {
-				currency: '<?php echo STP_VRS == 1 ? 'р' : '$' ?>',
+				currency: '<?php echo STP_VRS == 1 ? '<span class="glyphicon glyphicon-ruble"></span>' : '$' ?>',
 				timeOrigin: new Date('<?php echo \gmdate('Y-m-d\TH:i:sP') ?>'),
 				timeOffset: <?php echo DTIME_OFFSET/60 ?>,
 				allowTrade: <?php echo $summary['session']['allowTrade'] ?> 
 			}
 		</script>
-		<script type="text/javascript" src="/js/stp.js?x=<?php echo filemtime(Yii::getAlias('@app/web/js/stp.js')) ?>"></script>
+		<script type="text/javascript" src="/js/stp.min.js?x=<?php echo filemtime(Yii::getAlias('@app/web/js/stp.js')) ?>"></script>
 
 		<style>	
-			input, select, button, textarea {
-				border-radius: 2px!important;
-				-moz-border-radius: 2px!important;
-			}
-			textarea.input-lg, .input-lg {
-				font-size: 18px!important;
-			}				
-			
-			h2 {
-				font-size: 1.4em;
-				font-weight: 400;
-				text-transform: uppercase;		
-			}
-			h2 > span {
-				display: inline-block;
-				vertical-align: middle;				
-			}
-			h3 {
-				font-family: 'PT Sans Caption';
-				font-size: 20px;
-				font-weight: normal;
-			}			
-			h3 + .grey {
-				font-family: 'PT Sans Caption';
-			}
-			h3 + p {
-				font-size: 1.3em;
-			}
-
-			hr + .small {
-				margin-bottom: 0;
-				font-size: 1.1em;
-			}
-			
-			table {
-				margin-top: 0;
-			}
-			table > tbody > tr.darkblue > td {
-				padding-top: 4px;
-				padding-bottom: 4px;
-			}
-			table > tbody > tr.darkblue + tr > td {
-				padding-bottom: 20px;
-			}
-			
-			dl + div > .btn {
-				font-size: 1.2em;
-				margin-right: 40px;
-			}
-			dl > dt, dl > dd {
-				font-size: 1.3em;
-			}			
-			
-			form p, label {
-				font-size: 1.2em!important;
-			}
-			form .small {
-				font-size: 85%!important;
-			}
-			
-			label {
-				color: #6d757b;
-			}
-
-			ul > li .glyphicon, .glyphicon {
-				line-height: inherit;
-			}
-			
-			.icon-buy::before {
-				content: '▲';
-			}
-			.icon-sell::before {
-				content: '▼';
-			}
-			.btn-buy, .btn-buy:active, .btn-buy:focus, .btn-buy:active:focus {
-				color: #4CAF50;
-				background-color: inherit;
-				border: 1px solid #4CAF50;
-				font-size: 16px;
-				font-weight: bold;
-				outline: none;
-			}
-			.btn-sell, .btn-sell:active, .btn-sell:focus, .btn-sell:active:focus {
-				color: #d42c2c;
-				background-color: inherit;
-				border: 1px solid red;
-				font-size: 16px;
-				font-weight: bold;
-				outline: none;
-			}
-			.btn-buy:hover, .btn-buy:active  {
-				opacity: 0.7;
-				color: #4CAF50;
-			}
-			.btn-sell:hover, .btn-sell:active {
-				opacity: 0.7;
-				color: #d42c2c;
-			}
-			.btn-buy:disabled, .btn-sell:disabled {
-				opacity: 0.5;
-				color: #333;
-				border-color: #333;
-			}			
-			.red {
-				color: #d42c2c;
-			}
-			.green {
-				color: #4CAF50;
-			}
-
-			.mb0 {
-				margin-bottom: 0;
-			}
-			
 			.helper {
 				position: fixed;
 				top: 50%;
 				right: -1px;
 				margin-top: -30px;
-				z-index: 1;
+				z-index: 10;
 				box-shadow: 2px 5px 5px grey;
 			}
 			.helper * {
@@ -198,6 +84,26 @@
 			.helper span {
 				font-family: 'PT Sans', sans-serif;
 			}
+			@media (max-width: 760px) {
+				.helper {
+					position: fixed;
+					width: 100%;
+					top: 100%;
+					margin-top: -40px;
+					z-index: 10;
+					box-shadow: 2px 5px 5px grey;
+					opacity: 0.8;
+				}
+				.helper > .btn {
+					width: 100%;
+					text-align: center;
+					border: none!important;
+				}
+				.helper > .btn > * {
+					display: inline-block;
+					vertical-align: middle;
+				}					
+			}			
 
 			.ticket {
 				margin-bottom: 24px;
@@ -219,44 +125,11 @@
 			.glyphicon + .icon-group {
 				margin: 0 0 0 10px;
 			}
-			
-			.grey {
-				color: grey;
-			}
-			
+
 			.small > p:last-child {
 				margin-bottom: 0;
 			}
 			
-			.item-frame {
-				padding: 10px 15px;
-				font-size: .9em;
-				background-color: #f0f5f5;
-				border-top: 4px solid #dee1e1;
-			}
-			.dl-horizontal.item-frame > dt {
-				width: 110px;
-				font-weight: normal;
-				color: #333;
-				padding: 6px;
-			}
-			.dl-horizontal.item-frame > dd {
-				margin-left: 120px;
-				color: gray;
-				padding: 6px;
-			}
-			
-			.chart-box {
-				display: block;
-				position: relative;
-				color: inherit;
-				padding-bottom: 10px;
-			}
-			
-			.m-position span {
-				font-size: 1.1em;
-			}
-
 			.message {
 				padding: 20px 30px 10px 20px;
 				border: 1px solid #da7e0e;
@@ -286,113 +159,25 @@
 			}
 			.message.hidden + div {	
 				margin-top: 0;
-			}							
-			.news h3 + p {
-				margin-bottom: 0;
-			}		
-			.input-lg + .form-control-feedback {
-				height: auto!important;
-			}
-			.input-group > .form-control {
-				padding-right: 30px;
-			}
-			.form-control + .input-ok {
-				position: absolute;
-				right: 0;
-				padding: 15px;
-				z-index: 2;
-			}
-			.btn-md, .btn-group-md >.btn {
-				padding: 10px 12px;
-				font-size: 18px;
-			}			
-			.form-text:first-child {
-				margin-top: 10px;
 			}
 			
 			.toggle {
 				line-height: 1.5;
 			}
-			.dl-horizontal.nobr dt {
-				float: left;
-				width: 160px;
-				overflow: hidden;
-				clear: left;
-				white-space: nowrap;
-			}
-			
-			.monosign {
-				font-style: normal;
-				width: 0.6em;
-				text-align: right;
-				display: inline-block;
-			}
-			
-			.thumb:focus, .btn:focus {
-				text-decoration: none;
-				outline: none;
-			}
-			
-			.modal-box.graph-box {
-				background: none;
-				border: none;
-				box-shadow: none;
-			}
-			
+
 			.stp-header {
 				margin-bottom: 6px;
 			}				
 			.stp-header .h1 {
 				margin: 0;
-			}		
-			.stp-header .text, .stp-header p {
-				font-size: 1.4em;
 			}
 			.stp-header p {
 				line-height: 1.5;
 			}
 			.stp-header td > .grey {
 				display: block;
-			}			
-
-			.help-icon {
-				display: inline-block;
-				margin-left: 10px;
-				vertical-align: middle;
-				width: 24px;
-				height: 24px;
-				background: url('/ui/help.png') no-repeat center center;
-				cursor: pointer;
-				opacity: 0.3;
-				filter: alpha(opacity=30);
-			}
-			.help-icon:hover, .help-icon:active, .help-icon:focus {
-				opacity: 1;
-				filter: alpha(opacity=100);
-			}
-
-			.darkblue {
-				color: #aaa8a8;
-				text-transform: uppercase;
-				font-weight: 800;
-			}
-			.table-items td,
-			.table-items th {
-				border: 0!important;
-			}
-			.table-items tbody {
-				border-top: 1px solid #ddd;
-				border-bottom: 1px solid #ddd;
-				border-collapse: separate;
 			}
 			
-			.circle {
-				font-size: 1.6em;
-				line-height: 0.9em;
-				position: relative;
-				left: -4px;
-				top: 1px;
-			}	
 			.circle-green {
 				color: #68b19a;
 			}
@@ -403,70 +188,7 @@
 			.content > div {
 				margin-bottom: 1em;
 			}
-			
-			.nav-tabs {
-				font-size: 18px;
-			}
-			
-			.preview > li > a {
-				border: 1px solid #9E9089;
-				padding: 18px;
-				margin-bottom: 12px;
-				display: block;
-			}
-			.preview > li > div > .grey {
-				margin: 6px 0;
-			}			
-			.preview > li h4 {
-				margin-bottom: 0;
-				font-weight: normal;
-				height: 2.2em;
-				overflow: hidden;
-			}			
-			.preview > li p {
-				margin-bottom: 0;
-			}			
-			.preview > li p, .preview > li span {
-				font-size: 90%;
-			}
-			.preview .grey + div {
-				height: 3.8em;
-				overflow: hidden;
-				position: relative;
-			}
-			.preview .grey + div > p:first-child {
-				height: 66%;
-				overflow: hidden;
-			}
-			.preview .grey + div > p:only-child {
-				height: 100%;
-			}			
-			.preview .blur {
-				position: absolute;
-				display: block;
-				bottom: 20px;
-				background: white;
-				opacity: 0.6;
-				height: 10px;
-				z-index: 3;
-				width: 100%;
-			}
-			.preview .blue {
-				color: #428bca;
-				font-size: 100%;
-				font-weight: bold;
-			}
-			.preview .goto:hover .blue {
-				color: #254b6c;
-			}
-			
-			.card-info > li {
-				vertical-align: middle;
-			}
-
-			.sota {
-				font-size: 36px;
-			}			
+						
 			.sota img {
 				width: 2em;
 			}
@@ -477,27 +199,6 @@
 			
 			.nav > li > a {
 				padding: 8px 12px;
-			}
-			
-			.note {
-				background: #e4e8e8;
-				padding: 16px 16px 10px;
-				min-width: 320px;
-				font-size: 0.9em;
-				width: 70%;
-			}
-			.note h4 {
-				font-size: 1.1em;
-				font-weight: normal;
-			}			
-			.note h4 + ul {
-				padding-left: 20px;
-				margin: 0;
-			}
-			.note li {
-				list-style-type:square;
-				list-style-position: inset;
-				padding-bottom: 5px;
 			}
 			
 			.graph-label {
@@ -514,23 +215,7 @@
 				opacity: 0.6;
 				filter: alpha(opacity=60);
 			}
-			
-			.text-box {
-				max-width: 96%;
-				padding: 24px 30px;
-			}			
-			.text-box .small {
-				max-height: 510px;
-				overflow: auto;
-				font-size: 94%;
-			}
-			.text-box p:last-child {
-				margin-bottom: 0;
-			}
-			.text-box img {
-				max-width: 100%;
-			}
-			
+
 			span.glyphicon-ruble {
 				font-size: 75%;
 			}
@@ -542,253 +227,6 @@
 				opacity: 0.8;
 				filter: alpha(opacity=80);
 			}
-			
-			[type=radio] {
-				margin-top: 0.4em!important;
-			}
-			
-			.checkbox > label > *, .radio > label > * {
-				vertical-align: middle;
-				display: inline-block;
-				position: static!important;
-			}
-			
-						
-			.spreadsheet {
-				width: 98%;
-			}
-			.graph-box p {
-				color: #b1afaf;
-				font-size: 1.4em;
-				font-weigth: bold;
-				font-family: 'PT Sans Caption';
-			}			
-			.graph-box .btn-cover {
-				padding: 6px 12px;
-				background-color: transparent;
-				border: 1px solid #b1afaf;
-				color: #b1afaf;
-				margin-bottom: 18px;
-				font-size: 1.0em;
-			}
-			.graph-box .graph-outline {
-				border-top: 2px dashed #b1afaf;
-				border-bottom: 2px dashed #b1afaf;
-				height: 450px;
-				margin-bottom: 24px;
-				padding: 10px 0;
-				text-align: center;
-				overflow-y: auto;
-				max-width: 100%;
-			}
-			.graph-outline .img-xm {
-				max-height: 100%;
-				width: 1100px;
-			}
-			.graph-outline .img-xl {
-				max-height: 100%;
-				width: 1600px;
-			}
-			@media (min-height: 800px) {
-				.graph-box .graph-outline {
-					height: 600px;
-				}
-			}				
-			@media (min-height: 960px) {
-				.graph-box .graph-outline {
-					height: 800px;
-				}
-			}			
-			@media (max-height: 500px) {
-				.graph-box .graph-outline {
-					height: 360px;
-				}
-			}
-			
-			@media (max-width: 1400px) {
-				body {
-					font-size: 16px!important;
-				}
-				.sota {
-					font-size: 28px;
-				}
-				.menu:first-child, .content, .stp-header {
-					font-size: 14px!important;
-				}
-				.stp-header .text, .stp-header p {
-					font-size: 1.3em;
-				}				
-				.input-lg {
-					font-size: 16px!important;
-					padding: 8px 12px;
-					line-height: auto!important;
-				}
-				.btn-md, .btn-group-md >.btn {
-					font-size: 16px;
-				}
-				.form-control + .input-ok {
-					padding: 10px;
-				}
-				.nav-tabs .small {
-					font-size: 80%!important;		
-				}
-				
-				input.input-lg {
-					height: 36px!important;
-				}
-				select.input-lg {
-					height: 36px!important;
-					padding: 6px 12px;
-				}
-				form p, label, input, select {
-					font-size: 1.2em!important;
-				}				
-			}			
-			@media (max-width: 991px) {
-				.stp-header .auth {
-					margin-top: 2em;
-					border-top: 6px solid #e2e8e9;
-					border-bottom: 6px solid #e2e8e9;
-					padding-top: 12px;
-					padding-bottom: 6px;				
-				}
-				.stp-header .auth > div {
-					margin-right: 2em;
-				}				
-				.stp-header .auth > * {
-					display: inline-block;
-					vertical-align: middle;
-					line-height: 20px;
-					padding-bottom: 6px;
-				}
-				
-				.stp-header .sota {
-					margin: 0;
-					font-size: 200%;
-				}
-				.stp-header .sota, .sota + span {
-					display: inline-block;
-					vertical-align: middle;	
-				}
-				
-				.stp-header + .row {
-					display: none;
-				}
-				
-				.container {
-					margin-top: 40px;
-				}
-
-				.mobile {
-					display: block!important;
-				}
-				.menu {
-					margin: 0 auto;
-					background: none;
-					position: static;
-					display: block;
-				}				
-				.menu > li {
-					padding: 0;
-					display: block;
-				}
-				.menu a {
-					display: block;
-					margin: 12px auto;
-					padding: 6px;
-					font-size: 24px;
-					font-weight: 200;
-					border: none;
-				}	
-				.menu > li > a:hover {
-					color: white;
-					border-top: 1px solid white;
-					border-bottom: 1px solid white;
-					background: inherit;
-					padding: 5px 6px;
-				}
-				.opener {
-					display: block!important;
-				}
-			}
-			@media (max-width: 640px) {
-				.stp-header .text {
-					font-size: 1.2em;
-				}
-				.stp-header > div > *, .stp-header > div > .auth {
-					margin-top: 2em;
-				}
-				.stp-header > div > .sota {
-					margin-top: 1.1em!important;
-				}				
-				.stp-header .auth > * {
-					padding-bottom: 6px;
-				}
-				.stp-header .auth > div:first-child {
-					width: 75%;
-					margin-right: 0;
-				}
-				.stp-header .text.auth {
-					font-size: 1.3em;
-				}
-				p {
-					font-size: 125%;
-				}				
-			}
-			@media (max-width: 400px) {
-				body {
-					font-size: 15px;
-				}
-				.section {
-					padding-left: 0;
-					padding-right: 0;
-				}
-				.stp-header .text {
-					font-size: 1.0em;
-				}
-				.stp-header .sota {
-					font-size: 160%;
-				}				
-				.stp-header .text.auth {
-					font-size: 1.0em;
-				}
-				.stp-header > div > .sota {
-					margin-top: 1.6em!important;
-				}
-				.stp-header > div > * {
-					margin-top: 2.5em!important;
-				}
-				.circle {
-					font-size: 12px;
-					top: -1px;
-				}
-				.modal-box {
-					width: 96%;
-					padding: 12px;
-				}
-				.text-box .small {
-					max-height: 480px;
-				}
-			}			
-			@media (min-width: 800px) {
-				.text-box {
-					width: 800px!important;
-				}
-			}
-			@media (min-width: 1400px) {
-				.btn-md, .btn-group-md >.btn {
-					font-size: 20px;
-				}
-				.nav .small {
-					font-size: 95%;
-				}
-				.message p {
-					font-size: 0.99em;
-				}					
-				.message > .close, .message > .close:focus  {
-					font-size: 1.5em;
-				}				
-			}			
 		</style>
 	</head>
 
@@ -938,7 +376,7 @@
 							<p class="text-center"></p>
 						</div>
 						<div class="group ctrl text-center">
-							<button class="btn btn-primary btn-lg btn-close">&nbsp;&nbsp;&nbsp;OK&nbsp;&nbsp;&nbsp;</button>
+							<button class="btn btn-primary btn-md btn-close">&nbsp;&nbsp;&nbsp;OK&nbsp;&nbsp;&nbsp;</button>
 						</div>
 					</div>
 					
@@ -986,7 +424,7 @@
 						<br/>
 						
 						<div class="group ctrl text-center">
-							<button class="btn btn-primary btn-lg btn-close">&nbsp;Закрыть&nbsp;</button>
+							<button class="btn btn-primary btn-md btn-close">&nbsp;Закрыть&nbsp;</button>
 						</div>
 					</div>
 					
@@ -1056,8 +494,8 @@
 							</div>
 							
 							<div class="group ctrl">
-								<button class="btn btn-success btn-lg">Далее</button>
-								<button class="btn btn-default btn-lg btn-close">Отмена</button>
+								<button class="btn btn-success btn-md">Далее</button>
+								<button class="btn btn-default btn-md btn-close">Отмена</button>
 							</div>
 						</form>
 					</div>
